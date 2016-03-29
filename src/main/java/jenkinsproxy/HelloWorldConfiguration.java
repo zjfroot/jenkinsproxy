f790dfa9b7cc765ee0b18d261606c87269f3c9aa
Jifeng Zhang <zjfroot@gmail.com>
2016-03-29 18:49:29 +0200

    helloworld app

---
+++ src/main/java/jenkinsproxy/HelloWorldConfiguration.java
@@ -0,0 +1,34 @@
+package jenkinsproxy;
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+/**
+ * Created by jifeng.zhang on 29/03/16.
+ */
+public class HelloWorldConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+}
